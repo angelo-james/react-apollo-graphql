7e6476cc4d567052e4ed180f656ab5ed7d9f775b
AJ Arriola <angelojames.arriola@gmail.com>
2019-03-08 13:11:32 -0800

    created a new grapql object type for launches

--- schema.js
+++ schema.js
@@ -9,3 +9,14 @@ const {
   GraphQLSchema
 } = require('graphql');
 
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
+  })
+})
